75156df234b8237d6b8dd1a5b2ec7d8b0ca7b72b
Konstantinos Egkarchos <konsnosl@gmail.com>
2015-09-11 13:53:43 +0300

    Initial commit.

---
+++ Procedural Cave Generation/Procedural Cave Generation/Assets/MeshGenerator.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshGenerator : MonoBehaviour 
+{
+    public SquareGrid squareGrid;
+
+    public void GenerateMesh(int[,] map, float squareSize)
+    {
+        squareGrid = new SquareGrid(map, squareSize);
+    }
+
+    public class SquareGrid
+    {
+        public Square[,] squares;
+
+        public SquareGrid(int[,] map, float squareSize)
+        {
+
+        }
+    }
+
+    public class Square
+    {
+        public ControlNode topLeft, topRight, bottomRight, bottomLeft;
+        public Node centerTop, centerRight, centerBottom, centerLeft;
+
+        public Square (ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomLeft, ControlNode _bottomRight)
+        {
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomRight = _bottomRight;
+            bottomLeft = _bottomLeft;
+
+            centerTop = topLeft.right;
+            centerRight = bottomRight.above;
+            centerBottom = bottomLeft.right;
+            centerLeft = bottomLeft.above;
+        }
+    }
+
+    public class Node
+    {
+        public Vector3 position;
+        public int vertexIndex = -1;
+
+        public Node(Vector3 _pos)
+        {
+            position = _pos;
+        }
+    }
+
+    public class ControlNode : Node
+    {
+        public bool active;
+        public Node above, right;
+
+        public ControlNode(Vector3 _pos, bool _active, float squareSize) :base(_pos)
+        {
+            active = _active;
+            above = new Node(position + Vector3.forward * squareSize / 2f);
+            right = new Node(position + Vector3.right * squareSize / 2f);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Procedural-Cave-Generation
+
+A copy-paste implementation of the tutorial about Procedural Cave Generation from Unity3D website ( http://unity3d.com/learn/tutorials/projects/procedural-cave-generation-tutorial ).
